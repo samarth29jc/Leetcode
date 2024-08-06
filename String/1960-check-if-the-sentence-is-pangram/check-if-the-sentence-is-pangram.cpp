eb52e30aba2c9e2f4320b602fb55a15431cbffdc
Samarth Mishra <139863510+samarth29jc@users.noreply.github.com>
2024-08-06 23:47:05 +0530

    Time: 5 ms (18.74%) | Memory: 9.3 MB (5.78%) - LeetSync

---
+++ String/1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_map<char,int>mpp;
+        for(int i=0;i<sentence.size();i++){
+            mpp[sentence[i]]++;
+        }
+        if(mpp.size()!=26){
+          return false;
+        }
+        return true;
+    }
+};
