2c5213fabb17bf0e5b5e3e0dd7a3cf6a4652bb19
Samarth Mishra <139863510+samarth29jc@users.noreply.github.com>
2024-08-12 11:16:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (47.58%) - LeetSync

--- String/151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ String/151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        //story
-        //1. reverse whole string
-        
-        reverse(s.begin(), s.end());
-        
-        int i = 0;
-        //hero honge hamare l and r jo revrese karenge words ko
-        int l = 0, r = 0;
-        
-        int n = s.length();
-        
-        while(i < n) {
-            while(i < n && s[i] != ' ') { //i ko agar char dikha to r ko dega and i++ and r++
-                s[r++] = s[i++];
+        vector<string> v;
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]!=' '){
+                ans+=s[i];
             }
-            
-            if(l < r) { //l     r
-                reverse(s.begin()+l, s.begin()+r);
-                
-                s[r] = ' ';
-                r++;
-                
-                l = r;
+            else{
+                if(!ans.empty())
+                v.push_back(ans);
+                ans="";
             }
-            
-            i++; //y eto badhta rahega
         }
-        
-        s = s.substr(0 , r-1);
-        
-        return s;
-        
+         if(!ans.empty())
+         v.push_back(ans);
+     string result="";
+     for(int i=v.size()-1;i>=0;i--){
+         result+=v[i];
+         if(i!=0)
+         result+=' ';
+     }
+     return result;
     }
 };
