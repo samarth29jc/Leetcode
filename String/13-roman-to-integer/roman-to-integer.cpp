93b244b00d8a963156eb41a886e7ce882f124b7e
Samarth Mishra <139863510+samarth29jc@users.noreply.github.com>
2024-08-14 14:59:28 +0530

    Time: 7 ms (77.28%) | Memory: 10.9 MB (81.50%) - LeetSync

--- String/13-roman-to-integer/roman-to-integer.cpp
+++ String/13-roman-to-integer/roman-to-integer.cpp
@@ -1,26 +1,24 @@
+// Compare the input and return the correct value 
+int num(char c){
+    if(c=='I') return 1;
+    else if(c=='V') return 5;
+    else if(c=='X') return 10;
+    else if(c=='L') return 50;
+    else if(c=='D') return 500;
+    else if(c=='C') return 100;
+    else return 1000;
+}
+
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> m;
-        
-        m['I'] = 1;
-        m['V'] = 5;
-        m['X'] = 10;
-        m['L'] = 50;
-        m['C'] = 100;
-        m['D'] = 500;
-        m['M'] = 1000;
-        
-        int ans = 0;
-        
-        for(int i = 0; i < s.length(); i++){
-            if(m[s[i]] < m[s[i+1]]){
-                ans -= m[s[i]];
-            }
-            else{
-                ans += m[s[i]];
-            }
+        int n=s.size(); // size of the string
+        int ans=0; // it will calculate ans
+        for(int i=0;i<n-1;i++){ 
+            if(num(s[i])<num(s[i+1])) ans-=num(s[i]); // calling the num funtion and checking each value of the string whether if smaller then next or not if smaller then do "-" and else "+";
+            else ans+=num(s[i]);
         }
+        ans+=num(s[n-1]); // add remaining value as we run the loop from 0 to n-2;
         return ans;
     }
 };
