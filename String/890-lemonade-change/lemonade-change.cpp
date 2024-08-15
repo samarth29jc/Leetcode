345128ce85155113ab50743c3574770585434d53
Samarth Mishra <139863510+samarth29jc@users.noreply.github.com>
2024-08-15 06:14:08 +0530

    Time: 80 ms (22.48%) | Memory: 85.8 MB (81.41%) - LeetSync

--- String/890-lemonade-change/lemonade-change.cpp
+++ String/890-lemonade-change/lemonade-change.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int five = 0, ten = 0;  // Track the count of $5 and $10 bills
-        
-        for(int i = 0; i < bills.size(); i++) {
-            if(bills[i] == 5) {
-                five++;  // Customer pays with a $5 bill, no change needed
-            } else if(bills[i] == 10) {
-                if(five > 0) {  // Check if we have a $5 bill to give as change
-                    five--;
-                    ten++;
-                } else {
-                    return false;  // No $5 bill to give as change
-                }
-            } else if(bills[i] == 20) {
-                if(ten > 0 && five > 0) {  // Prefer giving one $10 bill and one $5 bill as change
-                    ten--;
-                    five--;
-                } else if(five >= 3) {  // Otherwise, give three $5 bills as change
-                    five -= 3;
-                } else {
-                    return false;  // Not enough bills to give correct change
-                }
-            }
+      int a[11];
+      a[5]=0;
+      a[10]=0;
+      for (int i=0; i<bills.size(); i++){
+        if (bills[i]==5){
+          a[5]++;
+        }else if (bills[i]==10){
+          if (a[5]==0) return false;
+          else {a[5]--; a[10]++;}
+        }else{
+          if (a[10]>0 && a[5]>0){
+            a[10]--; a[5]--;
+          }else if(a[5]>=3){
+            a[5]-=3;
+          } else 
+          return false;
         }
-        
-        return true;  // Successfully gave change to all customers
+      }
+      return true;
     }
 };
