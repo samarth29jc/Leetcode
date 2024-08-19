f8ef011f70c3543f966dacc455226e05101cbd0c
Samarth Mishra <139863510+samarth29jc@users.noreply.github.com>
2024-08-19 12:52:08 +0530

    Time: 701 ms (5.00%) | Memory: 179.8 MB (5.29%) - LeetSync

--- String/1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ String/1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -5,18 +5,15 @@ public:
         int res=0;
         for(int i=0;i<n;i++){
             map<char,int>mpp;
-           
+            multiset<int>st;
             for(int j=i;j<n;j++){
-                 int minn=INT_MAX;
-                 int maxx=INT_MIN;
+                if(mpp.find(s[j])!=mpp.end()){
+                    st.erase(st.find(mpp[s[j]]));
+                }
                  mpp[s[j]]++;
-                 for(auto it:mpp){
-               maxx=max(maxx,it.second);
-                minn=min(minn,it.second);
-               }
-                res+=maxx-minn;
+                 st.insert(mpp[s[j]]);
+                 res+=*st.rbegin()-*st.begin();
                 }      
-            
         }
      return res;
     }
