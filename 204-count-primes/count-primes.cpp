e94e0cdd5f2fcd0dc14927d8a8178554ddb73ea2
Samarth Mishra <139863510+samarth29jc@users.noreply.github.com>
2024-09-10 12:33:44 +0530

    Time: 129 ms (88.08%) | Memory: 29.4 MB (26.38%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<int> arr(n+1, 1);
-        int cnt=0;
-        
-        for(int i=2;i*i<=n;i++){
-            for(int j=i*i;j<=n;j+=i){
-                arr[j]=0;
+        int prime[n+1] ;
+        for(int i =2 ; i<n ; i++){
+            prime[i]=1;
+        }
+
+        for(int i =2 ; i*i<n ; i++){
+            if(prime[i]==1){
+                for(int j = i*i ; j<n ; j+=i){
+                    prime[j]=0;
+                }
             }
         }
-        for(int i=2;i<n;i++){
-            if(arr[i]==1){
-                cnt++;
-            }}
 
-    return cnt;
+        int cnt=0;
+        for(int i =2 ; i<n ; i++){
+           if(prime[i]==1) cnt++; 
+        }
+
+        return cnt;
+        
     }
 };
