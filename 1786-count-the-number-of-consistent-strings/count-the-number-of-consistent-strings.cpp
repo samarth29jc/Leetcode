e90fff966b5215361e277540432ccb756d358d2d
Samarth Mishra <139863510+samarth29jc@users.noreply.github.com>
2024-09-12 11:24:57 +0530

    Time: 40 ms (84.99%) | Memory: 34 MB (58.62%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.cpp
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-        vector<int>vpp;
-        int ans=0;
-      for(int i=0;i<allowed.size();i++){
-         vpp.push_back(allowed[i]);
-      }  
-      for(int i=0;i<words.size();i++){
-                    string word=words[i];
-            bool equal=true;
-        for(int j=0;j<word.size();j++){
-           
-            if(find(vpp.begin(),vpp.end(),word[j])==vpp.end()){
-                equal=false;
-            }
-           
-              
-
-            }
-             if(equal){
-                ans++;
-             }  
-            
+        int arr[26] = {0};
+        for (char c : allowed) {
+            arr[c - 'a'] = 1;
         }
-       
-      
-    
-    return ans;}
+        
+        int count = 0;
+        for (string word : words) {
+            int flag = 1;
+            for (char c : word) {
+                if (arr[c - 'a'] == 0) {
+                    flag = 0;
+                    break;
+                }
+            }
+            count += flag;
+        }
+        return count;
+    }
 };
