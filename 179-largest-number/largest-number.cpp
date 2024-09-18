a75e3c413ef6c38782fa6dab52539618fdcea1ac
Samarth Mishra <139863510+samarth29jc@users.noreply.github.com>
2024-09-18 10:46:57 +0530

    Time: 8 ms (48.02%) | Memory: 17.4 MB (14.46%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    string largestNumber(vector<int>& nums) {
+    static bool compare(const string &a, const string &b) {
+        return a + b > b + a;
+    }
     
-       auto myComp=[](int a,int b){
-        string s1=to_string(a);
-        string s2=to_string(b);
-        if((s1+s2)>(s2+s1)){
-            return true;
+    string largestNumber(vector<int>& nums) {
+        vector<string> strs;
+        for (int num : nums) {
+            strs.push_back(to_string(num));
         }
-        else return false;
-       };
-
-        sort(nums.begin(),nums.end(),myComp);
-         
-         if(nums[0]==0){
-            return "0";
-         }
-
-        string result="";
-        for(int num:nums){
-         result+=to_string(num);
+        
+        sort(strs.begin(), strs.end(), compare);
+        
+        // If the largest number is "0", return "0".
+        if (strs[0] == "0") return "0";
+        
+        string result;
+        for (string s : strs) {
+            result += s;
         }
+        
         return result;
     }
 };
