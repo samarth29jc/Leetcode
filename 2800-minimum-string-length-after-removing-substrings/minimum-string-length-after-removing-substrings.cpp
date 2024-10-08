d2834f20bbd5f81cef8b8f1258fc07e3622d23e6
Samarth Mishra <139863510+samarth29jc@users.noreply.github.com>
2024-10-08 10:00:08 +0530

    Time: 4 ms (75.53%) | Memory: 11.4 MB (73.63%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> stk;
+        for(int i=0; i<s.size(); i++){
+            if(!stk.empty() && ((s[i] == 'B' && stk.top() == 'A') || (s[i] == 'D' && stk.top() == 'C'))){
+                stk.pop();
+            }else{
+                stk.push(s[i]);
+            }
+        }
+        return stk.size();
+    }
+};
